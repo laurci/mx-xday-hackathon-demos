70a17e226486cd515caecc1f1668098c1bf42656
Andrei Pampu <pampu.s.andrei@gmail.com>
2023-10-20 02:01:28 +0300

    feat: add send nft script

---
+++ control/send-nft.sh
@@ -0,0 +1,9 @@
+mxpy --verbose contract call erd1vdtu708ntm7328yvptpesrtw6jzj2dke5jahulw8yh469rs8mjdqsffef4 \
+    --gas-limit 15000000 \
+    --pem ~/robot-wallet.pem \
+    --recall-nonce --proxy https://gateway.multiversx.com --chain M \
+    --function ESDTNFTTransfer \
+    --arguments str:ROBOTWARS-79fd78 0x4 0x1 $1 \
+    --send
+
+
